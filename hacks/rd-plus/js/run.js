4cd82efa1047ba26675df81c90a3a57a813bc98e
Glitch <terrariamods.scratch@gmail.com>
2024-11-29 01:32:43 -0700

    Load After Patch Applied

--- hacks/rd-plus/js/run.js
+++ hacks/rd-plus/js/run.js
@@ -9,15 +9,17 @@ const run = async () => {
             resolve();
         });
     }
-    await loadScript("https://raw.githubusercontent.com/TerrariaMods-Scratch/terrariamods-scratch.github.io/main/hacks/rd-plus/js/rd-plus-patch.js");
     await loadScript("https://raw.githubusercontent.com/TerrariaMods-Scratch/terrariamods-scratch.github.io/main/hacks/rd-plus/js/anti-cheat.js");
-    setProgress(1);
-    loadingScreen.hidden = true;
-    launchScreen.hidden = false;
-    launchScreen.addEventListener('click', () => {
-        launchScreen.hidden = true;
-        scaffolding.start();
-    });
-    launchScreen.focus();
+    window.RDPlusPatchCallback = () => {
+        setProgress(1);
+        loadingScreen.hidden = true;
+        launchScreen.hidden = false;
+        launchScreen.addEventListener('click', () => {
+            launchScreen.hidden = true;
+            scaffolding.start();
+        });
+        launchScreen.focus();
+    };
+    await loadScript("https://raw.githubusercontent.com/TerrariaMods-Scratch/terrariamods-scratch.github.io/main/hacks/rd-plus/js/rd-plus-patch.js");
 };
 run().catch(handleError);
