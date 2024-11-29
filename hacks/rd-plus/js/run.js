3d8f027de8b91997feb28d91c7809f96577f7fba
Glitch <terrariamods.scratch@gmail.com>
2024-11-29 01:16:19 -0700

    Update Loading Screen

--- hacks/rd-plus/js/run.js
+++ hacks/rd-plus/js/run.js
@@ -1,6 +1,8 @@
 const run = async () => {
     const projectData = await getProjectData();
     await scaffolding.loadProject(projectData);
+    await loadScript("https://raw.githubusercontent.com/TerrariaMods-Scratch/terrariamods-scratch.github.io/main/hacks/rd-plus/js/rd-plus-patch.js");
+    await loadScript("https://raw.githubusercontent.com/TerrariaMods-Scratch/terrariamods-scratch.github.io/main/hacks/rd-plus/js/anti-cheat.js");
     setProgress(1);
     loadingScreen.hidden = true;
     launchScreen.hidden = false;
